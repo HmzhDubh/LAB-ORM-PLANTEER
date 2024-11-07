5547e0174101d79b63436a977244861da127ac3f
HmzhDubh <meezo.2015@hotmail.com>
2024-11-07 11:16:18 +0300

    complete edit plant view

--- Planteer/plantApp/urls.py
+++ Planteer/plantApp/urls.py
@@ -5,6 +5,6 @@ app_name = 'plantApp'
 urlpatterns = [
     path('all/', views.all_plants_view, name='all_plants_view'),
     path('new/', views.new_plant_view, name='new_plant_view'),
-    path('<plant_id>/details/', views.plant_details_view, name="plant_details_view")
-
+    path('<plant_id>/details/', views.plant_details_view, name="plant_details_view"),
+    path('<plant_id>/update/', views.update_plant_view, name="update_plant_view"),
 ]
--- Planteer/plantApp/views.py
+++ Planteer/plantApp/views.py
@@ -10,11 +10,12 @@ def all_plants_view(request: HttpRequest):
     plants = Plant.objects.all()
     return render(request, 'all_plants.html', context={'plants':plants})
 
+
 def plant_details_view(request: HttpRequest, plant_id:int):
-    print(plant_id)
+
     try:
         plant = Plant.objects.get(pk=plant_id)
-        print(plant.name)
+
         request = render(request, 'plant_details.html', context={'plant': plant})
         return request
     except Exception as e:
@@ -35,11 +36,28 @@ def new_plant_view(request: HttpRequest):
         )
 
         new_plant.save()
-        print(request.POST)
+        
         request = redirect('plantApp:all_plants_view')
         return request
 
     request = render(request, 'new_plant.html')
     return request
 
+def update_plant_view(request: HttpRequest, plant_id):
 
+    plant = Plant.objects.get(pk=plant_id)
+
+    if request.method == "POST":
+
+        plant.name = request.POST['name']
+        plant.about = request.POST['about']
+        plant.used_for = request.POST['used_for']
+        plant.category = request.POST['category']
+        plant.is_edible = request.POST['is_edible']
+
+        if 'image' in request.FILES: plant.image = request.FILES['image']
+        plant.save()
+
+        return redirect('plantApp:plant_details_view', plant_id=plant.id)
+
+    return render(request, 'update_plant.html', context={'plant': plant})
